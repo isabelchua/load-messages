029ad86003210454036586ef6a4b826b66bdbf9d
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-09-01 13:01:44 -0700

    added context

--- src/App.js
+++ src/App.js
@@ -21,13 +21,11 @@ const reducer = (state, action) => {
 	}
 };
 
-function App() {
-	// loading
-	// more
-	// after
-	// data
-	//const [data] = React.useState(allData.slice(0, perPage));
+//const [data] = React.useState(allData.slice(0, perPage));
 
+const MyContext = React.createContext();
+
+function MyProvider({ children }) {
 	const [state, dispatch] = React.useReducer(reducer, {
 		loading: false,
 		more: true,
@@ -37,37 +35,59 @@ function App() {
 
 	const { loading, data, after, more } = state;
 
+	const load = () => {
+		dispatch({ type: 'start' });
+
+		setTimeout(() => {
+			const newData = allData.slice(after, after + perPage);
+			dispatch({ type: 'loaded', newData });
+		}, 300);
+	};
+	return (
+		<MyContext.Provider value={{ loading, data, more, load }}>
+			{children}
+		</MyContext.Provider>
+	);
+}
+
+function App() {
 	return (
 		<div className="App">
-			<ul>
-				{data.map(row => (
-					<li key={row} style={{ background: 'orange' }}>
-						{row}
-					</li>
-				))}
-				{loading && more && <li>Loading...</li>}
-				{!loading && (
-					<li style={{ background: 'green' }}>
-						<button
-							onClick={() => {
-								dispatch({ type: 'start' });
-
-								setTimeout(() => {
-									const newData = allData.slice(
-										after,
-										after + perPage
-									);
-									dispatch({ type: 'loaded', newData });
-								}, 500);
-							}}
-						>
-							Load More
-						</button>
-					</li>
-				)}
-			</ul>
+			<MapOut />
 		</div>
 	);
 }
 
-export default App;
+function MapOut() {
+	const { data, loading, more } = React.useContext(MyContext);
+
+	return (
+		<ul>
+			{data.map(row => (
+				<li key={row} style={{ background: 'orange' }}>
+					{row}
+				</li>
+			))}
+			{loading && <li>Loading...</li>}
+			{!loading && more && <LoadMore />}
+		</ul>
+	);
+}
+
+function LoadMore() {
+	const { load } = React.useContext(MyContext);
+
+	return (
+		<li style={{ background: 'green' }}>
+			<button onClick={load}>Load More</button>
+		</li>
+	);
+}
+
+export default () => {
+	return (
+		<MyProvider>
+			<App />
+		</MyProvider>
+	);
+};
